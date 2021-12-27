718e94d0fdf969e34a1ac6fc60f9dcdf6f2e1f55
kshyun0415 <kshyun0415@ewhain.net>
2021-12-28 03:58:20 +0900

    added
    added12,result,home

---
+++ splashscene1/Assets/Scripts/ClickGo.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ClickGo : MonoBehaviour
+{
+    public void SceneChange()
+    {   if (ClickAnimals.SelectedAnimal != -1){SceneManager.LoadScene("HomeScene");}
+    }
+}
+
