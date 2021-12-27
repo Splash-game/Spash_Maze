b50dbd4bbef06938ed877c925cf323bdb94114a6
JiYoung <dlwldud1151@naver.com>
2021-12-27 18:20:57 +0900

    mkj01

---
+++ makingJam/Assets/Scenes/GoToHome.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GoToHome : MonoBehaviour
+{
+    public void SceneChange()
+    {
+        SceneManager.LoadScene("HomeScene");
+    }
+}
