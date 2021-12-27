e463c7fc96b9a2e6c83ba573b94c4c13ff444ccb
JiYoung <dlwldud1151@naver.com>
2021-12-28 00:25:41 +0900

    3

---
+++ makingJam/Assets/Scenes/CharacterView.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterView : MonoBehaviour
+{
+    public GameObject BabyRabbit; //selection 0일때 
+    public GameObject BabyCat; // 1
+    public GameObject BabyBird; // 2
+
+    public int selection = 0;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (selection == 0) //토끼
+        {
+            BabyRabbit.SetActive(true);
+            BabyCat.SetActive(false);
+            BabyBird.SetActive(false);
+        }
+        else if (selection == 1) //고양이
+        {
+            BabyRabbit.SetActive(false);
+            BabyCat.SetActive(true);
+            BabyBird.SetActive(false);
+        }
+        else if (selection == 2) //새 
+        {
+            BabyRabbit.SetActive(false);
+            BabyCat.SetActive(false);
+            BabyBird.SetActive(true);
+        }
+
+    }
+}
